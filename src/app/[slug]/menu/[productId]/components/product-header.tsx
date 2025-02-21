a6c81af755c2ca901ae2b49b316951c09a8c213b
Gustavo Orelio <oreliogustavo@gmail.com>
2025-02-21 00:13:16 -0300

    feat: add product details page

--- src/app/[slug]/menu/[productId]/components/product-header.tsx
+++ src/app/[slug]/menu/[productId]/components/product-header.tsx
@@ -8,37 +8,37 @@ import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
 
 interface ProductHeaderProps {
-    product: Pick<Product, "name" | "imageUrl">
+  product: Pick<Product, "name" | "imageUrl">
 }
 
 const ProductHeader = ({ product }: ProductHeaderProps) => {
-    const router = useRouter()
-    const handleBackClick = () => router.back()
-    return (
-        <div className="relative h-[300px] w-full">
-            <Button
-                variant="secondary"
-                size="icon"
-                className="absolute left-4 top-4 z-50 rounded-full"
-                onClick={handleBackClick}
-            >
-                <ChevronLeftIcon />
-            </Button>
-            <Image
-                src={product.imageUrl}
-                alt={product.name}
-                fill
-                className="object-contain" />
-            <Button
-                variant="secondary"
-                size="icon"
-                className="absolute right-4 top-4 z-50 rounded-full"
-            >
-                <ScrollTextIcon />
-            </Button>
+  const router = useRouter()
+  const handleBackClick = () => router.back()
+  return (
+    <div className="relative h-[300px] w-full">
+      <Button
+        variant="secondary"
+        size="icon"
+        className="absolute left-4 top-4 z-50 rounded-full"
+        onClick={handleBackClick}
+      >
+        <ChevronLeftIcon />
+      </Button>
+      <Image
+        src={product.imageUrl}
+        alt={product.name}
+        fill
+        className="object-contain" />
+      <Button
+        variant="secondary"
+        size="icon"
+        className="absolute right-4 top-4 z-50 rounded-full"
+      >
+        <ScrollTextIcon />
+      </Button>
 
-        </div>
-    );
+    </div>
+  );
 }
 
 export default ProductHeader;
